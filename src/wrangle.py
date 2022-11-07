65ae0e847ae3c7d0f320b07a4eff486c51460cd8
Chris Rosenberger <jcrosenberger@pm.me>
2022-11-07 12:11:50 -0600

    I broke the wrangle module, it wasn't delivering the correct dataset. Fixed it. Started scaling exercise

--- src/wrangle.py
+++ src/wrangle.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 
 # My env module
@@ -20,7 +21,7 @@ def sql_zillow_2017():
     
     SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips, propertylandusetypeid
     FROM properties_2017
-    # JOIN propertylandusetype USING (propertylandusetypeid)
+    JOIN propertylandusetype USING (propertylandusetypeid)
     WHERE propertylandusetypeid = 261
     ''' 
     
@@ -28,8 +29,10 @@ def sql_zillow_2017():
     df = pd.read_sql(sql_query, env.codeup_db('zillow'))
     
     # this is a very big dataset, so we handle null values by dropping them
-    df = df.isnull().sum()
+    # df = df.isnull().sum()
     
+    # Cache data
+    df.to_csv('data/zillow_2017.csv')
     
     return df
 
@@ -48,10 +51,51 @@ def acquire_zillow_2017():
         
     else: 
         
-        df = sql_zillow_2017():
+        df = sql_zillow_2017()
     
+    # this is a very big dataset, so we handle null values by dropping them
+    # df = df.isnull().sum()
+
     # Cache data
-        df.to_csv('data/zillow_2017.csv')
+    #    df.to_csv('data/zillow_2017_dropped_nulls.csv')
     
     
     return df
+
+
+def wrangled_zillow_2017():
+    '''
+    This is a very large dataset and the values can get very wide so we will handle null values by dropping them.
+    The process will be to first turn whitespace into null values and then drop rows with null values from columns 
+    which we find to be important.  
+    '''
+
+
+    # checks to see if wrangled zillow data exists already
+    # if it does, then fills df with the stored data
+    if os.path.isfile('data/wrangled_zillow_2017.csv'):
+
+        df = pd.read_csv('data/wrangled_zillow_2017.csv')
+
+
+    else:
+        df = acquire_zillow_2017()
+
+        
+
+        # fills whitespace will Naans
+        df = df.replace(r'^\s*s', np.NaN, regex=True)
+
+        # the columns which we want to drop naan values from
+        drop_columns = ['calculatedfinishedsquarefeet', 'taxamount', 'yearbuilt', 'taxvaluedollarcnt']
+
+
+        # drop naans based on the columns identified above
+        df = df.dropna(subset = drop_columns)
+
+        # Cache data
+        df.to_csv('data/wrangled_zillow_2017.csv')
+
+
+
+    return df 
